9b96763fb40330455630971241f874aa9a352e8d
7eeyou7oon <vova.doronin3@gmail.com>
2026-06-01 20:35:56 +0500

    fix css/php

--- index.php
+++ index.php
@@ -885,6 +885,17 @@ if (
         }
 
     });
+
+  // Шапка при скролле
+    window.addEventListener('scroll', () => {
+        const header = document.querySelector('.header');
+
+        if (window.scrollY > 50) {
+            header.classList.add('scrolled');
+        } else {
+            header.classList.remove('scrolled');
+        }
+    });
     </script>
 
 </body>
